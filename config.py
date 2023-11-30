6c718ad2d29f7c9d9a2efbab240110115ff8cef6
Abdenour26 <151867814+Abdenour26@users.noreply.github.com>
2023-11-30 09:49:06 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 # Fichier de configuration pour les clés API et autres paramètres
-
-BINANCE_API_KEY = 'clé_api'
-BINANCE_SECRET_KEY = 'clé_secrète'
+# Nom de l'api "Test1"
+BINANCE_API_KEY = '6pa5ynjsxMY973774d3kxq4mxtfLTsMhrGaTwAR694FvZHxCx1Lf3cOwReozTP6z'
+BINANCE_SECRET_KEY = 'UijL03VdcegBP5MYOlaGAgKiAwVu9wAQDAKYBdmLLB0PwjjexNz8lZh6KBHd4Va6'
